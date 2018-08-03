9e52b71b3fa942806cb5125236ebc72de9c29a32
AndreiaSandovalGomes <gomesandreia27@outlook.com>
2018-08-03 17:16:09 -0300

    incluido marcadores

--- js/app.js
+++ js/app.js
@@ -1,29 +1,94 @@
 $(document).ready(function() {
-  $('.mainScreen').delay('5000').fadeIn('slow')
+  $('.mainScreen').delay('3000').fadeIn('slow')
 });
-var marker;
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
-    center: {lat: -25.344, lng: 131.036}
+    center: {lat: -23.561751, lng: -46.658998}
   });
 
-  marker = new google.maps.Marker({
-    map: map,
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    position: {lat: -25.344, lng: 131.036}
-  });
-  marker.addListener('click', toggleBounce);
+  // Marcadores no mapa
+  var lat;
+  var lng;
+  var image;
+  var type;
+  var description;
+  var name;
+
+  for(var i = 0; i < restaurantes.length; i++){
+    lat = restaurantes[i]['latitude'];
+    lng = restaurantes[i]['longitude'];
+    image = restaurantes[i]['image'];
+    description = restaurantes[i]['description'];
+    type = restaurantes[i]['type'];
+    name = restaurantes[i]['name'];
+
+      var marker = new google.maps.Marker({
+      map: map,
+      draggable: false,
+      animation: google.maps.Animation.DROP,
+      position: {lat, lng}
+      });
+  }
+  foodSelector();
 }
-function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
+
+function foodSelector(){
+  var Menu = document.getElementById("menu");
+  menu.innerHTML = "";
+  var types = document.createElement("option");
+  types.value = "none";
+  types.innerHTML = "Escolha o restaurante";
+  menu.appendChild(types);
+  for (i = 0; i < restaurantes.length; i++){
+    name = restaurantes[i]['name'];
+    var foodOption = document.createElement("option");
+    foodOption.value = name;
+    foodOption.innerHTML = name;
+    Menu.appendChild(foodOption);
   }
 }
+menu.addEventListener("change", imagesRest);
+function imagesRest(){
+  var rest = document.getElementById("rest");
+  rest.innerHTML = "";
+  for (i = 0; i < restaurantes.length; i++){
+    // console.log(restaurantes[i]);
+    if(menu.value == restaurantes[i]["name"]) {
+      console.log("lll");
+      image = restaurantes[i]['image'];
+      var img = document.createElement("img");
+      img.src = restaurantes[i]['image'];
+      rest.appendChild(img);
+      // img.setAttribute(class, valor)
+      // img.setAttribute(data-toggle, valor)
+      // img.setAttribute(data-target, valor)
+      // conteudo do modal
+    }
+  }
+}
+
+
+
+
+// $('.filter').click(function () {
+ //   var inputValue = $('textImput').val();
+ //   $.each(names, function(index, val) {
+ //     if(value !== inputValue) {
+ //       $(this).fadeOut('slow');
+ //     }
+ //   });
+ // })
+  // marker.addListener('click', toggleBounce);
+
+// function toggleBounce() {
+//   if (marker.getAnimation() !== null) {
+//     marker.setAnimation(null);
+//   } else {
+//     marker.setAnimation(google.maps.Animation.BOUNCE);
+//   }
+// }
 
 // function drop() {
 //   clearMarkers();
